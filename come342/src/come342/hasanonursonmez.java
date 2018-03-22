9aa7dec105d06b9aaf287e7d76edff732c67d3aa
hasan onur sönmez <hasanonur.6@gmail.com>
2018-03-22 22:56:24 +0300

    Hello world

---
+++ come342/src/come342/hasanonursonmez.java
@@ -0,0 +1,8 @@
+package come342;
+
+public class hasanonursonmez {
+	public static void main(String[] args) {
+	    // Prints "Hello, World" to the terminal window.
+	    System.out.println("Hello, World");
+	}
+}
